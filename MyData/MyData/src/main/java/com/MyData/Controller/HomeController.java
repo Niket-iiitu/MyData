236b0f768f50f86faadbe17d9904e15895c01f8b
Niket-iiitu <niketagrawal777@gmail.com>
2025-04-12 17:07:54 +0530

    Frontend integrated

---
+++ MyData/MyData/src/main/java/com/MyData/Controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.MyData.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping(value = "/ping")
+    private String pingCheck(){
+        return "Server Active";
+    }
+}
---
+++ MyData/MyData/src/main/java/com/MyData/MyDataApplication.java
@@ -0,0 +1,14 @@
+package com.MyData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyDataApplication.class, args);
+        System.out.println("Application Started ....");
+    }
+
+}
